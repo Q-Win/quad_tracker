3f1d4c05fbc5c489c48c60d27c382b6657aed79b
Quinn Krug <quinn.krug@gmail.com>
2020-05-03 15:03:23 -0600

    serving data to views

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,5 +1,14 @@
 class DataController < ApplicationController
 
+  def home
+    match_data = Match.all
+    @matches = match_data.order(start_time: :desc)
+    @wins = match_data.where(win: true).count
+    @losses = match_data.where(win: false).count
+    @win_percent = (@wins.to_f)/(@matches.count.to_f)*100
+
+  end
+
   def updatematches
     riot_service = RiotService.new('RGAPI-ed53936c-cd12-4761-ab31-f940cddcc4e9')
     riot_service.update_matches
