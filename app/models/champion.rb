f02e3b7c2b18746f3d4c2dc9fa729468af40524a
Quinn Krug <quinn.krug@gmail.com>
2020-05-05 09:06:31 -0600

    champion relationship tests are passing

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -1,4 +1,6 @@
 class Champion < ApplicationRecord
   validates :name, presence: true
   validates :champ_id, presence: true
+  has_many :match_champions
+  has_many :matches, through: :match_champions
 end
