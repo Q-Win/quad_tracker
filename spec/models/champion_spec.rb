971f16414ad6d573d081877da28e9971f8df3668
Quinn Krug <quinn.krug@gmail.com>
2020-05-05 09:05:14 -0600

    set up champion relationship tests

--- spec/models/champion_spec.rb
+++ spec/models/champion_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Champion, type: :model do
     it { should validate_presence_of(:name)}
     it { should validate_presence_of(:champ_id)}
   end
+  describe 'Relationship' do
+    it { should have_many(:match_champions) }
+    it { should have_many(:matches).through(:match_champions)}
+  end
 end
